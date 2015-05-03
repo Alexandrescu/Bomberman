7c9c06fb16e465854460f5f910be4848ac5e0287
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-05-03 12:48:56 +0100

    [#2] Merge fixes

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 var app = angular.module('mapApp', []);
 
 app.controller('KeyMapping', ['$scope', 'mapLevel', '$timeout', function($scope, mapLevel, $timeout) {
+  /*
+      Functie de debug. Cand asta e activata pot sa creez harti!
+   */
   $scope.debugMode = false;
   var markingValue = 5;
   $scope.mark = function(x, y) {
@@ -13,34 +16,42 @@ app.controller('KeyMapping', ['$scope', 'mapLevel', '$timeout', function($scope,
     }
   };
 
+  /*
+      Functia care este trigger-uita cand apas Level 1/2
+   */
   $scope.changeLevel = function(level) {
+    // Atributele lui Bijoux-Yoshi
     $scope.hasGlasses = false;
     $scope.hasMagicKey = false;
     $scope.yoshiCarrots = 0;
-
     $scope.won = false;
 
+    // Inglobarea hartii
     $scope.level = level;
     mapLevel.setLevel($scope.level);
 
     $scope.gameBoard = mapLevel.map;
 
-    $scope.heroX = mapLevel.start.x;
-    $scope.heroY = mapLevel.start.y;
-    $scope.cycleDirection = 0;
-
-    $scope.totalCarrots = mapLevel.countCarrots();
-
     $scope.boardSize = {
       height: $scope.gameBoard.length,
       width : $scope.gameBoard[0].length
     };
+
+    $scope.totalCarrots = mapLevel.countCarrots();
+
+    // Coordonatele + pozitia fetei
+    $scope.heroX = mapLevel.start.x;
+    $scope.heroY = mapLevel.start.y;
+    $scope.cycleDirection = 0;
   };
 
+  // Nivelul initial
   $scope.changeLevel(2);
 
+  // Dimensiune temporara a unui cell
   $scope.cellSize = 40;
 
+  // Event-ul trigger-uit atunci cand apas un buton
   $scope.buttonPress = function($event) {
     $scope.press = $event.keyCode;
     $scope.key = String.fromCharCode($scope.press);
@@ -50,6 +61,7 @@ app.controller('KeyMapping', ['$scope', 'mapLevel', '$timeout', function($scope,
     move(normalize($scope.press));
   };
 
+  // Traduceri din/inspre obiecte
   var up = {x : 0, y : -1};
   var down = {x : 0, y : 1};
   var left = {x : -1, y : 0};
@@ -66,6 +78,7 @@ app.controller('KeyMapping', ['$scope', 'mapLevel', '$timeout', function($scope,
     40 : down
   };
 
+  // Intoarce o directie pentru tasta respectiva
   function normalize(keyCode) {
     if(keyMap.hasOwnProperty(keyCode)) {
       return keyMap[keyCode];
@@ -75,6 +88,7 @@ app.controller('KeyMapping', ['$scope', 'mapLevel', '$timeout', function($scope,
     }
   }
 
+  // Marginea hartii
   function bound(number, property) {
     var upperBound = $scope.boardSize[property];
     if(number >= upperBound) return upperBound - 1;
@@ -82,8 +96,10 @@ app.controller('KeyMapping', ['$scope', 'mapLevel', '$timeout', function($scope,
     return number;
   }
 
+  // Yoshi are pozitii intr-o miscare
   var cyclePositions = 5;
 
+  // Directia in care trebuie sa se uite
   function moveMeBaby(direction) {
     switch(direction) {
       case up:
@@ -98,6 +114,7 @@ app.controller('KeyMapping', ['$scope', 'mapLevel', '$timeout', function($scope,
     }
   }
 
+  // Functia care il face pe Yoshi sa cicleze
   function cycleMe() {
     $timeout(function() {
       $scope.currentCycle = ($scope.currentCycle + 1) % cyclePositions;
@@ -107,13 +124,17 @@ app.controller('KeyMapping', ['$scope', 'mapLevel', '$timeout', function($scope,
     }, 30);
   }
 
+  // Incepem de pe mijlocul imaginii
   $scope.currentCycle = 3;
   function move(directions){
+    // Atunci cand castig ma opresc.
     if($scope.won) {return;}
     $scope.cycleDirection = moveMeBaby(directions);
     $scope.currentCycle = 4;
+    // Incepem animatia
     cycleMe();
 
+    // Computam ultima si actuala locatie
     var oldX = $scope.heroX;
     var oldY = $scope.heroY;
 
@@ -128,12 +149,15 @@ app.controller('KeyMapping', ['$scope', 'mapLevel', '$timeout', function($scope,
     var event = {};
 
     switch (boardValue) {
+      // Patratica goala
       case 0:
         event = {valid: true};
         break;
+      // Patratica invalida
       case -1:
         event = {valid:false};
         break;
+      // Patratica care merge in apa
       case  1:
         if($scope.hasGlasses) {
           event = {valid:true};
@@ -142,21 +166,25 @@ app.controller('KeyMapping', ['$scope', 'mapLevel', '$timeout', function($scope,
           event = {valid: false, msg: "You need the blue mushroom."};
         }
         break;
+      // Patratica care are morcov
       case 2:
         $scope.yoshiCarrots += 1;
         $scope.gameBoard[newY][newX] = 0;
         event = {valid : true, msg: "Ate carrot! Nom nom!"};
         break;
+      // Patratica care are swimming mushroom
       case 3:
         $scope.hasGlasses = true;
         $scope.gameBoard[newY][newX] = 0;
         event = {valid : true, msg: "Got the swimming mushroom! We can swim!"};
         break;
+      // Patratica care are cheie
       case 4:
         $scope.hasMagicKey = true;
         $scope.gameBoard[newY][newX] = 0;
         event = {valid: true, msg: "Got the key! Collect all carrots and finish the game!"};
         break;
+      // Patratica care are locatia finala
       case 5:
         if($scope.totalCarrots - $scope.yoshiCarrots == 0 && $scope.hasMagicKey) {
           event = {valid : true, msg: "You WON!"};
@@ -181,7 +209,7 @@ app.controller('KeyMapping', ['$scope', 'mapLevel', '$timeout', function($scope,
     $scope.yoshiLog = event.msg;
   }
 
-  // Taken from angular js website
+  // De pe AngularJS
   function simpleKeys (original) {
     return Object.keys(original).reduce(function (obj, key) {
       obj[key] = typeof original[key] === 'object' ? '{ ... }' : original[key];
--- app/mapBoard.js
+++ app/mapBoard.js
@@ -4,14 +4,13 @@ app.directive('mapBoard', function() {
   return {
     templateUrl: '/board',
     link: function(scope, element, attrs) {
-      console.log(element[0].firstElementChild.clientWidth);
       var parent = element[0].parentElement;
       var gameDiv = element[0].firstElementChild;
 
+      // Am grija harta sa stea pe pagina, pentru ca pagina sa nu se miste
       scope.cellSize = Math.min(
         (gameDiv.clientWidth) / scope.boardSize.width,
         parent.clientHeight / scope.boardSize.height);
-      console.log(scope.cellSize);
     }
   }
 });
